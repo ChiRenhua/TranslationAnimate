e99b5739d116fcdad13fbf6e9f2961f6e7b532a6
RenhuaChi <renhuachi@tencent.com>
2017-04-19 09:40:09 +0800

    Initial Commit

---
+++ TranslationAnimate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TranslationAnimate
+//
+//  Created by Renhuachi on 2017/4/19.
+//  Copyright © 2017年 tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
